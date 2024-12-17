f6bcc2a6a103c307a2ba35177c8b85a9ae3987b0
nmillorut <nmillorut@gmail.com>
2024-12-17 17:09:00 +0700

    lab9_1.cpp completed

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -3,10 +3,14 @@
 using namespace std;
 
 int main(){	
+	double moneyl,rate,moneyp,total,PB,interest,nb;
 	cout << "Enter initial loan: ";
+	cin >> moneyl;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
-
+	cin >> moneyp;
+	
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
@@ -18,16 +22,41 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
+	int i=0;
+	while (true) {
+		PB=moneyl;
+		interest=moneyl*(rate/100);
+		total=moneyl+interest;
+		nb=total-moneyp;
+
+		if (nb>0){
+			moneyl=nb;
+			cout << fixed << setprecision(2); 
+			cout << setw(13) << left << i+1; 
+			cout << setw(13) << left << PB;
+			cout << setw(13) << left << interest;
+			cout << setw(13) << left << total;
+			cout << setw(13) << left << moneyp;
+			cout << setw(13) << left << nb;
+			cout << "\n";
+			++i;
+		}
+		else if(nb<0){
+			if (total<moneyp){moneyp=total;}
+			nb=0;
+			cout << fixed << setprecision(2); 
+			cout << setw(13) << left << i+1; 
+			cout << setw(13) << left << PB;
+			cout << setw(13) << left << interest;
+			cout << setw(13) << left << total;
+			cout << setw(13) << left << moneyp;
+			cout << setw(13) << left << nb;
+			cout << "\n";
+			break;
+		}
+	}
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+
 	return 0;
 }
